3871e93c5808a4095c77dd7e8ab3458b0fb5049a
Nick Smith <ironsmith86@gmail.com>
2016-12-04 15:05:21 +0000

    Added javascript concat to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require("gulp"),
     gutil = require("gulp-util"),
     sass = require("gulp-sass"),
-    jsconcat = require("gulp-concat"),
+    concat = require("gulp-concat"),
     browserify = require("gulp-browserify"),
     connect = require("gulp-connect");
 
@@ -12,3 +12,18 @@ var htmlSources,
 htmlSources = ["builds/development/*.html"];
 sassSources = ["components/sass/*.sass"];
 jsSources = ["components/scripts/*.js"];
+
+gulp.task("connect", function(){
+    connect.server({
+        root: "builds/development/",
+        livereload: true
+    })
+});
+
+gulp.task('jsconcat', function(){
+    gulp.src(jsSources)
+        .pipe(concat("script.js"))
+        .pipe(browserify())
+        .pipe(gulp.dest("builds/development/js/"))
+        .pipe(connect.reload());
+})
