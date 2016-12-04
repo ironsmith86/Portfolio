b4b43f0e984a18cb67c3e9c29379547789034f2a
Nick Smith <ironsmith86@gmail.com>
2016-12-04 14:51:02 +0000

    Installed packages for gulp and set up gulp variables

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,14 @@
-var gulp = require('gulp');
+var gulp = require("gulp"),
+    gutil = require("gulp-util"),
+    sass = require("gulp-sass"),
+    jsconcat = require("gulp-concat"),
+    browserify = require("gulp-browserify"),
+    connect = require("gulp-connect");
+
+var htmlSources,
+    sassSources,
+    jsSources;
+
+htmlSources = ["builds/development/*.html"];
+sassSources = ["components/sass/*.sass"];
+jsSources = ["components/scripts/*.js"];
