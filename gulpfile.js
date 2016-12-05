c6a9b42a4b271783cd9801aff81deab8d49c77c8
Nick Smith <ironsmith86@gmail.com>
2016-12-05 21:52:38 +0000

    Fixed gulp-sass task and added babel pipe to jsconcat

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,40 @@
-var gulp = require("gulp"),
-    gutil = require("gulp-util"),
-    sass = require("gulp-sass"),
-    concat = require("gulp-concat"),
-    browserify = require("gulp-browserify"),
-    connect = require("gulp-connect");
+const gulp = require('gulp'),
+    babel = require('gulp-babel');
+    gutil = require('gulp-util'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    browserify = require('gulp-browserify'),
+    connect = require('gulp-connect');
 
 var htmlSources,
     sassSources,
     jsSources;
 
-htmlSources = ["builds/development/*.html"];
-sassSources = ["components/sass/*.sass"];
-jsSources = ["components/scripts/*.js"];
+htmlSources = ['builds/development/*.html'];
+sassSources = ['components/sass/style.scss'];
+jsSources = ['components/scripts/*.js'];
 
-gulp.task("connect", function(){
+gulp.task('connect', function(){
     connect.server({
-        root: "builds/development/",
+        root: 'builds/development/',
         livereload: true
     })
 });
 
+gulp.task('sass', function(){
+    gulp.src(sassSources)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('builds/development/css/'))
+        .pipe(connect.reload());    
+})
+
 gulp.task('jsconcat', function(){
     gulp.src(jsSources)
-        .pipe(concat("script.js"))
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(concat('script.js'))
         .pipe(browserify())
-        .pipe(gulp.dest("builds/development/js/"))
+        .pipe(gulp.dest('builds/development/js/'))
         .pipe(connect.reload());
 })
